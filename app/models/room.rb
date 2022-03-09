6af514cbb5c53c85cce34bb9ec072f54c8391a59
rosaliemorgan <rosalie.morgan@phe.gov.uk>
2022-03-09 17:09:21 +0000

    Add DeckAssignment intermediary model

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,9 @@
 class Room < ApplicationRecord
   has_many :rounds
+
+  has_one :deck_assignment, dependent: :destroy
+
+  has_one :deck, -> { readonly }, through: :deck_assignment
+
+  has_many :cards, -> { readonly }, through: :deck
 end
