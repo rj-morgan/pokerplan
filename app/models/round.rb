77313b25a090c7274389d0b3cc7bdd72532eaa3b
rosaliemorgan <rosalie.morgan@phe.gov.uk>
2022-03-10 12:41:11 +0000

    Added score aggregation and calculation methods

--- app/models/round.rb
+++ app/models/round.rb
@@ -4,4 +4,36 @@ class Round < ApplicationRecord
   has_many :votes
 
   delegate :deck, :cards, to: :room
+
+  def mean_score
+    return nil if scores.empty?
+
+    (scores.sum / scores.count).round(1) # more than 1dp doesn't make sense
+  end
+
+  def median_score
+    return nil if scores.empty?
+
+    mid = scores.count / 2
+    scores.count.odd? ? scores[mid] : (scores[mid.floor] + scores[mid.ceil]) / 2
+  end
+
+  def modal_score
+    return nil if scores.empty?
+
+    modes = frequencies.select { |_v, n| n == frequencies.values.max }
+    vals = modes.map { |v, _n| v.to_s }
+    vals.join(', ')
+  end
+
+  # hash of score counts
+  def frequencies
+    return nil if scores.empty?
+
+    scores.group_by(&:itself).transform_values(&:count)
+  end
+
+  def scores
+    votes.map(&:score).compact.sort
+  end
 end
